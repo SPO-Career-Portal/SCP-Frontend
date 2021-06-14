c42071c32e524f5d3be2d9e753a0dbd9b6533d5c
amanydv9131 <77360927+amanydv9131@users.noreply.github.com>
2021-06-15 00:19:05 +0530

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -14,6 +14,20 @@ var routes = [
     component: Index,
     layout: "/admin",
   },
+  {
+    path: "/login",
+    name: "Login",
+    icon: "ni ni-key-25 text-info",
+    component: Login,
+    layout: "/auth",
+  },
+  {
+    path: "/register",
+    name: "Register",
+    icon: "ni ni-circle-08 text-pink",
+    component: Register,
+    layout: "/auth",
+  },
 
   {
     path: "/user-profile",
