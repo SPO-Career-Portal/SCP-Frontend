fa96a6de9a63e520f40ac0911b1c5454ff41024d
Navneet Singh <78998610+navneet-28@users.noreply.github.com>
2021-07-19 00:47:31 +0530

    integration of registeration api  (#20)
    * integration of registeration api
    
    * minor changes
    
    * updated registration integration
    
    * handled duplicate registration
    
    * update

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,4 +1,19 @@
 import { sessionService } from "redux-react-session";
+import axios from "axios";
+
+const base_url = "http://localhost:8000"
+
+
+const sendRequest = async (url, method, body, headers = {}) => {
+  const response = await axios({
+    method,
+    url: base_url + url,
+    data: body,
+    headers,
+  });
+  console.log(response);
+  return response.data;
+};
 
 export const login = (username) => async () => {
   try {
@@ -14,20 +29,36 @@ export const login = (username) => async () => {
   }
 };
 
-export const register = (email,password) => async ()  => {
+export const register = (roll) => async ()  => {
     try{
-      const response={
-        email : email,
-        password : password
-      };
-
-      sessionService.saveSession();
-      sessionService.saveUser(response);
-      console.log("response", response);
+      const roll_no=roll;
+      
+      return sendRequest("/user/register", "POST", {
+        roll_no,
+      });
     } catch (err) {
       console.log("Error while registering!");
     }
 };
+
+export const setPass = (password) => async ()  => {
+  try{
+    const pass=password;
+    const result= sendRequest("/user/register/verify/code=<str:token>/", "POST", {
+      pass,
+    });
+    if(result=="200_OK"){
+      sessionService.saveSession();
+      sessionService.saveUser(result);
+    }
+    else{
+      alert("Error while registering");
+    }
+
+  } catch (err) {
+    console.log("Error while registering!");
+  }
+}; 
 
 export const logout = () =>
   async () => {
@@ -40,4 +71,3 @@ export const logout = () =>
       // error
     }
   };
-  
